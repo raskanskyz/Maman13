6cd4a9754aa61270044718982faaf0982a0c9c7e
Eran Raskansky <raskanskyzjunk@gmail.com>
2012-11-30 00:52:15 +0200

    Method progress

--- Maman13/src/RGBImage.java
+++ Maman13/src/RGBImage.java
@@ -29,7 +29,7 @@ public class RGBImage {
 		setWidth(image.getWidth());
 	}// CTOR3
 		// -----------------------------------------------------------------------------
-
+ 
 	/******************** METHODS BELOW *******************************/
 	/* OKAY */public int getHeight() {
 		return height;
